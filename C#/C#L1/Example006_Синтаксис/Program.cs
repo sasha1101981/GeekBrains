a797502ca9840756e0910582d8d93103d7ab1b31
Alexander Fedorov <sasha1101981@mail.ru>
2023-02-16 23:13:50 +0700

    E003S

---
+++ C#/C#L1/Example006_Синтаксис/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ввведите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.Write("Ура, этоже МАША");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
