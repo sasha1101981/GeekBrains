017292f27809ce4df920db46033265a9eb6de234
sasha1101981 <124509399+sasha1101981@users.noreply.github.com>
2023-02-25 00:00:48 +0700

    HW22

--- C#/C#L1/Example008_Magic/Program.cs
+++ C#/C#L1/Example008_Magic/Program.cs
@@ -18,21 +18,21 @@
 
     int count = 0;
 
-    while(count< 10000);
+    while(count< 10000)
     {
         int what = new Random().Next(0, 3);
-        if(what == 0);
+        if(what == 0)
         {
                 x = (x + xa) / 2;
                 y = (y + ya) / 2;
         }
 
-        if (what == 1);
-        {
+        if (what == 1)
+              {
                 x = (x + xb) / 2;
                 y = (y + yb) / 2;
         }
-        if (what == 2);
+        if (what == 2)
         {
                 x = (x + xc) / 2;
                 y = (y + yc) / 2;
