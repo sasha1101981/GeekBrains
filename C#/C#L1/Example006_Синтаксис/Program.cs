71882e39b202db6601b47563dd0c5ef82e34c9ed
sasha1101981 <124509399+sasha1101981@users.noreply.github.com>
2023-03-04 19:19:58 +0700

    12

--- C#/C#L1/Example006_Синтаксис/Program.cs
+++ C#/C#L1/Example006_Синтаксис/Program.cs
@@ -1,12 +1,18 @@
-﻿Console.Write("Ввведите имя пользователя: ");
-string username = Console.ReadLine();
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Ввведите имя пользователя: ");
+        string username = Console.ReadLine();
 
-if(username.ToLower() == "маша")
-{
-    Console.Write("Ура, этоже МАША");
-}
-else
-{
-    Console.Write("Привет, ");
-    Console.WriteLine(username);
+        if (username.ToLower() == "маша")
+        {
+            Console.Write("Ура, этоже МАША");
+        }
+        else
+        {
+            Console.Write("Привет, ");
+            Console.WriteLine(username);
+        }
+    }
 }
